f6dcf83d5b2d1180e328e783b74d8c975eb9e7a9
V4lar <yassin.trader111@gmail.com>
2026-06-21 19:10:55 +0300

    Update logo image size in Navigation component

--- src/components/krftist/Navigation.jsx
+++ src/components/krftist/Navigation.jsx
@@ -39,7 +39,7 @@ export default function Navigation() {
             src="/logo1.png"
             alt="KRFTIST logomark"
             data-testid="nav-logomark"
-            className="w-20 h-20 md:w-28 md:h-28 object-contain shrink-0"
+            className="w-32 h-32 md:w-40 md:h-40 object-contain shrink-0"
             style={{ mixBlendMode: "screen" }}
           />
           
